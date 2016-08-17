5289c10471e5acb68ee3bbbf3cd52d69a050bd5e
sheng.li <sheng.li@feisystems.com>
2016-08-17 13:47:43 -0400

    change brand from mhc to c2s
    #10467 - As a developer, I will complete rebranding changes in code

--- discovery-server/src/main/java/gov/samhsa/c2s/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/gov/samhsa/c2s/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package gov.samhsa.mhc.discoveryserver;
+package gov.samhsa.c2s.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
